18b94fb69cf5157b7e93ea1a8c8e0f8a67cba89c
lgkinter <lgkinter@yahoo.com>
2016-06-20 19:17:14 -0400

    Revert "Oops! Test..."
    This reverts commit 4de804a0a4db37f84b2597ac0dbafae1ab5df83e.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,72 +1,35 @@
-class TicTacToe
-
-  WINNER_LIST = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-  # no good comment
-  
+class Game
   def initialize
     @board = []
+    player1 = Player.new
     @board.each_with_index {|item, index| @board[index] = index}
-    @winner = nil
   end
 
   def print_board
-    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "#{@board[0]} | #{@board[1]} | #{P@board[2]}"
     puts "-----------"
-    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "#{@board[3]} | #{@board[0]} | #{@board[0]}"
     puts "-----------"
-    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+    puts "#{@board[0]} | #{@board[0]} | #{@board[0]}"
   end
 
   def play
-    start_game
-    loop do
-      if winner?
-        puts "#{@winner.name} wins!"
-        print_board
-        return
-      elsif draw?
-        puts "It's a draw."
-        print_board
-        return
-      else
-        make_move
-        print_board
-      end
-  end
-
-  def start_game
     print "First player, please enter your name: "
-    @player1 = Player.new(gets.chomp, 'x')
+    player1 = Player.new(gets.chomp, 'x')
     puts "#{player1}, your marker will be 'x'."
     print "Second player, please enter your name: "
-    @player2 = Player.new(gets.chomp, 'o')
+    player2 = Player.new(gets.chomp, 'o')
     puts "#{player2}, your marker will be 'o'."
-    @current_player = rand(1) == 0 ? player1 : player2
+
     puts "Let's get started! #{current_player.name} you have been randomly selected to go first."
+    print_board
   end
 
-  def get_move
-    print "#{current_player.name}, enter any available position between 0 and 8: "
-    gets.chomp
+  def start_Game
+    rand(1) == 0 ? current_player = player1
   end
-
-  def valid_move?
-
-  end
-
   def make_move
-
-  end
-  def winner?
-    WINNER_LIST.each do |win_set|
-      if @board[win_set[0]] == @board[win_set[1]] && @board[win_set[0]] == @board[win_set[2]]
-        @winner = @board[win_set[0]] == "x" ? @player1 : @player2
-      end
-    end
-  end
-
-  def draw?
-    @board.all? {|square| square == 'x' || square == 'o'}
+    print "#{current_player.name}, enter any available position between 1 and 9: "
   end
 end
 
